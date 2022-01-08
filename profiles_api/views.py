9f5a3eaf03e0202021998c75b72e37589cf41b65
Valerii Stefanyk <valeriistefanyk@gmail.com>
2022-01-08 19:11:51 +0200

    Added searching to user profile API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import authentication
 from rest_framework.views import APIView
-from rest_framework import serializers, viewsets
+from rest_framework import viewsets, filters
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import status
@@ -96,3 +96,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
